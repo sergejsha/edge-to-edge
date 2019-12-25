e6f674fad065dce847b5c296bdd055774f95503b
Sergej Shafarenka <beworker@gmail.com>
2019-12-25 19:12:44 +0100

    Update libraries

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -21,8 +21,7 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
-    implementation("androidx.appcompat:appcompat:1.0.2")
-    implementation("androidx.core:core-ktx:1.0.2")
+    implementation("androidx.appcompat:appcompat:1.1.0")
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
